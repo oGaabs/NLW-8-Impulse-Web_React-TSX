7d7bffeec0065e8901ba41c56094e96a04b4e5e7
Gabriel Santana <gabriel04.ok@gmail.com>
2022-05-22 00:53:51 -0300

    🔧 | New response code

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -10,18 +10,27 @@ export const routes = express.Router()
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body
 
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
-        prismaFeedbacksRepository,
-        nodemailerMailAdapter
-    )
+    try
+    {
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository()
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+            prismaFeedbacksRepository,
+            nodemailerMailAdapter
+        )
 
-    await submitFeedbackUseCase.execute({
-        type,
-        comment,
-        screenshot
-    })
+        await submitFeedbackUseCase.execute({
+            type,
+            comment,
+            screenshot
+        })
 
-    return res.status(201).send()
+        return res.status(201).send()
+    }
+    catch (error) {
+        console.log(error)
+
+        return res.status(500).send()
+    }
+
 })
